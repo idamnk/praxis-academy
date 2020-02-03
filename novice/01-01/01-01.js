8b23a25d1041e6d8e669fbd2176dcc8a2a88c639
idamnk <idamnk64@gmail.com>
2020-02-03 10:52:03 +0700

    create file 01-01.js

---
+++ novice/01-01/01-01.js
@@ -0,0 +1,22 @@
+// bunding
+let caught = 5 * 5;
+
+let ten = 10;
+console.log(ten * ten);
+
+let mood = "light";
+console.log(mood);
+
+mood = "dark";
+console.log(mood);
+
+let luigisDebt = 140;
+luigisDebt = luigisDebt - 35;
+console.log(luigisDebt);
+
+let one = 1, two = 2;
+console.log(one + two);
+
+var name = "Ayda";
+const greeting = "Hello ";
+console.log(greeting + name);
