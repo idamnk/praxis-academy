106cd3036f3ffc732334e7f70db42b74799a26bc
idamnk <idamnk64@gmail.com>
2020-02-05 14:38:01 +0700

    day2

---
+++ novice/01-01/Untitled-1.js
@@ -0,0 +1,11 @@
+let nilai = 40
+if(nilai <60){
+    console.log('Nilai anda ambyar')
+}
+else if (nilai >=60 && nilai <80){
+    console.log('Nilai anda B')
+}
+else if (nilai >=80 && nilai <100){
+    console.log('Nilai anda A')
+}
+else {console.log('Nilai mu kapiken')}
---
+++ novice/01-02/01-02.js
@@ -0,0 +1,39 @@
+//Defining a function
+const square = function(x) {
+    return x * x;
+  };
+  
+  console.log(square(12));
+
+const makeNoise = function() {
+  console.log("Pling!");
+};
+
+makeNoise();
+
+const power = function(base, exponent) {
+  let result = 1;
+  for (let count = 0; count < exponent; count++) {
+    result *= base;
+  }
+  return result;
+};
+
+console.log(power(2, 10));
+
+//Nested scope
+const hummus = function(factor) {
+    const ingredient = function(amount, unit, name) {
+      let ingredientAmount = amount * factor;
+      if (ingredientAmount > 1) {
+        unit += "s";
+      }
+      console.log(`${ingredientAmount} ${unit} ${name}`);
+    };
+    ingredient(1, "can", "chickpeas");
+    ingredient(0.25, "cup", "tahini");
+    ingredient(0.25, "cup", "lemon juice");
+    ingredient(1, "clove", "garlic");
+    ingredient(2, "tablespoon", "olive oil");
+    ingredient(0.5, "teaspoon", "cumin");
+  };
